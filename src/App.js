3f978e68e90b67ab2df595c6836b795194f58b7b
M02! <moriel.pev@gmail.com>
2021-05-05 23:19:11 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Fetch from "./components/Fetch";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <Fetch />
       </header>
