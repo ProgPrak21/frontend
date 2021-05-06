216a25afb8a5ed572532f3ae794cb87ad9026359
M02! <moriel.pev@gmail.com>
2021-05-06 22:30:27 +0200

    added Home Component ,Background vid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Fetch from "./components/Fetch";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <header className="App-header">
         <Fetch />
       </header>
---
+++ src/components/Home.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import vid from './video/video.mp4';
+import {
+    makeStyles,
+  } from "@material-ui/core";
+
+  const useStyles = makeStyles((theme) => ({
+    
+    button:{
+        color:"#5aadaa", 
+        backgroundColor:"black",
+        marginLeft:"5px",
+        fontSize:"20px",
+        border: "none",
+        fontWeight:"bold"
+        
+    },
+    text:{
+        color:"white",
+        
+        
+        fontWeight:"bold",
+        fontSize:"30px"
+    },
+    title:{
+        color:"#5aadaa",
+        
+    },
+    home:{
+        height: '100vh',
+        minHeight : '100vh'
+    }
+    
+  }));
+   
+const Home = () =>{
+    const classes = useStyles();
+    
+        return(<div className={classes.home} >
+            <video
+            autoPlay
+            loop
+            muted
+            style={{
+                position:"absolute",
+                width:"100%",
+                left:"50%",
+                top:"50%",
+                height:"100%",
+                objectFit:"cover",
+                transform:"translate(-50%,-50%)",
+                zIndex:"-1"
+            } }>
+                <source src= {vid} type="video/mp4"/>
+            </video>
+            <h1 className={classes.title}>Why Should You Care?</h1>
+            
+            <p className={classes.text}>
+            ad nulla adipisicing consequat ex ut id quis aliquip elit nostrud consequat
+             reprehenderit magna et duis Lorem culpa adipisicing do do aliqua est ex excepteur  
+             ipsum ut labore reprehenderit consectetur dolore pariatur mollit mollit 
+             exercitation anim quis do enim sint exercitation excepteur amet id consequat 
+             
+             non sit dolor in irure pariatur enim veniam eu fugiat non labore dolore irure
+              cupidatat mollit officia consectetur dolore et dolor incididunt do aliqua
+               fugiat et pariatur consectetur reprehenderit fugiat pariatur commodo
+               
+               ad nulla adipisicing consequat ex ut id quis aliquip elit nostrud consequat
+             reprehenderit magna et duis Lorem culpa adipisicing do do aliqua est ex excepteur 
+             consectetur do cupidatat eu non nulla ut enim labore in eu ut fugiat cupidatat 
+             laborum cupidatat et aute tempor nostrud fugiat cupidatat minim consequat 
+             ipsum ut labore reprehenderit consectetur dolore pariatur mollit mollit 
+             exercitation anim quis do enim sint exercitation excepteur amet id consequat 
+             incididunt ea nisi adipisicing quis nostrud proident culpa
+             non sit dolor in irure pariatur enim veniam eu fugiat non labore dolore irure
+              cupidatat mollit officia consectetur dolore et dolor incididunt do aliqua
+               fugiat et pariatur consectetur reprehenderit fugiat pariatur commodo 
+               
+
+            </p>
+            <button className={classes.button}>click here</button>
+            
+            </div>)
+    
+
+}
+export default Home;
