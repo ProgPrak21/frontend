03cdd7bb04cfef16ae93b2c63249b872b1ad6646
M02i <moriel.pev@gmail.com>
2021-06-25 18:51:41 +0200

    changes website to single page, fixed navbar buttons functionalty

---
+++ src/components/Analyze.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  
+}));
+
+const Analyze = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      
+    </div>
+  );
+};
+
+export default Analyze;
