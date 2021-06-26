70da3bfc43d3758fea299882fbc8c4f8806d9e17
majedidilbi <majed.idlby@gmail.com>
2021-06-26 12:35:21 +0200

    update gql query

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,8 +1,15 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { GET_USER_DATA } from "../graphql/get-user-data";
+// import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Button } from "@material-ui/core";
+import { useState } from "react";
 
 export default function UserData() {
-  const { loading, error, data } = useQuery(GET_USER_DATA, {
+  const [userId, setUserId] = useState("");
+  const [secret, setSecret] = useState("");
+
+  const [getData, { loading, error, data }] = useLazyQuery(GET_USER_DATA, {
+    variables: { secret, userId },
     onCompleted: () => {
       console.log(data);
     },
@@ -14,5 +21,26 @@ export default function UserData() {
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
 
-  return <div style={{ color: "white" }}>data will be here!</div>;
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <form noValidate autoComplete="off">
+          <TextField
+            onChange={({ target }) => setUserId(target.value)}
+            label="User ID"
+            variant="outlined"
+          />
+          <TextField
+            onChange={({ target }) => setSecret(target.value)}
+            label="Secret"
+            variant="outlined"
+          />
+        </form>
+        <Button onClick={() => getData()} color="primary" variant="contained">
+          Analyse
+        </Button>
+      </div>
+      <div style={{ color: "white" }}>data will be here!</div>
+    </div>
+  );
 }
--- src/graphql/get-user-data.js
+++ src/graphql/get-user-data.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_USER_DATA = gql`
-  query UserData {
-    UserData {
+  query UserData($secret: String, $userId: String) {
+    UserData(secret: $secret, userId: $userId) {
       userId
       company
       topic
